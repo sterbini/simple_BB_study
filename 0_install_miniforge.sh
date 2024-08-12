608bd249f1bb51c700fada5ed68e4e3fc2422c62
Guido Sterbini <guido.sterbini@cern.ch>
2024-08-12 16:30:03 +0200

    Status

--- 0_install_miniforge.sh
+++ 0_install_miniforge.sh
@@ -2,7 +2,7 @@ wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge
 bash Miniforge3-Linux-x86_64.sh -b  -p ./miniforge -f
 rm -f Miniforge3-Linux-x86_64.sh
 source miniforge/bin/activate
-python -m pip install ipython numpy scipy pandas psutil cpymad xsuite
+python -m pip install ipython numpy scipy pandas psutil cpymad xsuite nafflib
 mkdir modules
 cd modules
 git clone https://github.com/xsuite/tree_maker.git
@@ -13,7 +13,7 @@ cd xmask/
 git submodule init
 git submodule update
 cd ../
-git clone --recurse-submodules https://github.com/xsuite/example_DA_study.git
+git clone --recurse-submodules --branch runIII https://github.com/xsuite/example_DA_study.git 
 cd ../
 cp -r ./modules/example_DA_study/studies/template_jobs/* .
 python edit_configs.py
--- edit_configs.py
+++ edit_configs.py
@@ -6,7 +6,7 @@ yaml = YAML()
 # open yaml file in ./1_build_distr_and_collider/config.yaml
 
 with open('./1_build_distr_and_collider/config.yaml') as file:
-    config = yaml.load(file, Loader=yaml.FullLoader)
+    config = yaml.load(file)
 
 # %%
 config['config_mad']['links']['acc-models-lhc']='../modules/example_DA_study/external_dependencies/acc-models-lhc'
@@ -17,7 +17,7 @@ with open('./1_build_distr_and_collider/config.yaml', 'w') as file:
 # %%
 
 with open('./2_configure_and_track/config.yaml') as file:
-    config = yaml.load(file, Loader=yaml.FullLoader)
+    config = yaml.load(file)
 
 # %%
 config['config_collider']['config_beambeam']['mask_with_filling_pattern']['pattern_fname']='../modules/example_DA_study/studies/filling_scheme/8b4e_1972b_1960_1178_1886_224bpi_12inj_800ns_bs200ns.json'
